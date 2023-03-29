637676dc56d61de3fb2b29c802a0d75e3be8efca
Justin Andrew <justin.nicholas@eslabs.in>
2023-03-29 16:57:17 +0530

    question changes

--- lib/QuizQuestions/quizQuestions.dart
+++ lib/QuizQuestions/quizQuestions.dart
@@ -8,6 +8,43 @@ class QuizQuestions extends StatefulWidget {
 }
 
 class _QuizQuestionsState extends State<QuizQuestions> {
+  int initialIndex = 0;
+
+  Map questions = {
+    "questions": [
+      {
+        "question": "What is 1+1?",
+        "answer": ["1", "2", "3", "4"],
+        "correctAnswer": "2",
+      },
+      {
+        "question": "What is 2+2?",
+        "answer": ["1", "2", "3", "4"],
+        "correctAnswer": "4",
+      },
+      {
+        "question": "What is Capital of India?",
+        "answer": ["Chennai", "Banglore", "Delhi", "Mumbai"],
+        "correctAnswer": "Delhi",
+      },
+      {
+        "question": "What is Capital of Tamilnadu?",
+        "answer": ["Chennai", "Banglore", "Delhi", "Mumbai"],
+        "correctAnswer": "Delhi",
+      },
+      {
+        "question": "What is Capital of Karnataka?",
+        "answer": ["Chennai", "Banglore", "Delhi", "Mumbai"],
+        "correctAnswer": "Delhi",
+      },
+      {
+        "question": "What is Capital of Maharastra?",
+        "answer": ["Chennai", "Banglore", "Delhi", "Mumbai"],
+        "correctAnswer": "Delhi",
+      },
+    ]
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,34 +53,19 @@ class _QuizQuestionsState extends State<QuizQuestions> {
       ),
       body: Column(
         children: [
-           SizedBox(height: 20, width: 400),
-           Text('1 - What is the Answer for 1234 % 10'),
-           SizedBox(height: 20),
-            Row(
-              children: [
-                 SizedBox(width: 100),
-                ElevatedButton(onPressed: () {},
-                 child: Text('4'),
-            ),
-             SizedBox(width: 50),
-                ElevatedButton(onPressed: () {},
-                 child: Text('1'),
-            ),
-              ],
-            ),
-             SizedBox(height: 20),
-            Row(
-              children: [
-                 SizedBox(width: 100),
-                ElevatedButton(onPressed: () {},
-                 child: Text('3'),
-            ),
-             SizedBox(width: 50),
-                ElevatedButton(onPressed: () {},
-                 child: Text('2'),
-            ),
-              ],
-            ),
+          Text(questions["questions"][initialIndex]["question"]),
+          for (int i = 0;
+              questions["questions"][initialIndex]["answer"].length > i;
+              i++)
+            Text(questions["questions"][initialIndex]["answer"][i]),
+          ElevatedButton(
+              onPressed: () {
+                if (initialIndex != questions["questions"].length - 1) {
+                  initialIndex++;
+                  setState(() {});
+                }
+              },
+              child: Text("Next"))
         ],
       ),
     );
